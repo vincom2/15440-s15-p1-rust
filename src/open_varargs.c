cac1b536cbf000b092975529bb9631545c170a2f
Vincent Huang <vincom2@gmail.com>
2015-03-09 07:50:14 -0400

    deleting shitcode
    (except not really because that would entail deleting the entire repo,
    HA HA HA!)

--- src/open_varargs.c
+++ src/open_varargs.c
@@ -7,16 +7,6 @@
 int open_create(const char *pathname, int flags, mode_t m);
 int open_nocreate(const char *pathname, int flags);
 
-/*int open_create(const char *pathname, int flags, mode_t mode) {
-    printf("whatevski man\n");
-    return 3;
-}
-
-int open_nocreate(const char *pathname, int flags) {
-    printf("whatever man\n");
-    return 4;
-}*/
-
 int open(const char *pathname, int flags, ...) {
     mode_t m = 0;
     if(flags & O_CREAT) {
